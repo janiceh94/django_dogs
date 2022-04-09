64e57ab863867e7eda3080102828e5e70b660ad0
janiceh94 <haesung94@gmail.com>
2022-04-09 12:40:07 -0700

    imported dog model

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-
+from .models import Dog
 
 # Create your views here.
 
@@ -11,29 +11,12 @@ class Home(TemplateView):
 class About(TemplateView): 
     template_name = 'about.html'
 
-#fakebase
-class Dog:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-#fakebase
-dogs = [
-    Dog("Odie", 36, "Male"),
-    Dog("Blue", 26, "Female"),
-    Dog("Spike", 31, "Male"),
-    Dog("Snoopy", 72, "Male"),
-    Dog("Pluto", 92, "Male"),
-    Dog("Scoopy-Doo", 53, "Male"),
-    Dog("Courage", 26, "Male")
-]
-
 class Dog_List(TemplateView):
     template_name = 'dog_list.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["dogs"] = dogs
+        context["dogs"] = Dog.objects.all()
         return context
 
 
