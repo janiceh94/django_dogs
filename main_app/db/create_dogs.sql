74540661cc75779f46ae54d8356c860bd1d049fc
janiceh94 <haesung94@gmail.com>
2022-04-09 12:38:10 -0700

    migrated files, created admin, created seed data

---
+++ main_app/db/create_dogs.sql
@@ -0,0 +1,20 @@
+INSERT INTO main_app_dog(name, img, age, gender, created_at)
+VALUES ('Odie', 'https://static.wikia.nocookie.net/great-characters/images/a/a8/Odiethedog.gif/revision/latest?cb=20200628132323', 36, 'male', '03-19-2022');
+
+INSERT INTO main_app_dog(name, img, age, gender, created_at)
+VALUES ('Blue', 'https://static.wikia.nocookie.net/thebluescluesencyclopedia/images/7/7e/Ddspbme-fbdb8dee-8961-4f1b-9419-dd19bc76f801_2.png/revision/latest/scale-to-width-down/250?cb=20200605005009', 26, 'female','03-19-2022');
+
+INSERT INTO main_app_dog(name, img, age, gender, created_at)
+VALUES ('Spike', 'https://static.wikia.nocookie.net/nickelodeon/images/2/2f/Oil_Painted_Spike.png/revision/latest?cb=20190827034055', 31, 'male','03-19-2022');
+
+INSERT INTO main_app_dog(name, img, age, gender, created_at)
+VALUES ('Snoopy', 'https://www.calmuseums.org/images/snoopy/snoopy.png', 72, 'male','03-19-2022');
+
+INSERT INTO main_app_dog(name, img, age, gender, created_at)
+VALUES ('Pluto', 'https://static.wikia.nocookie.net/characters/images/a/ac/DSNY-SM-13.jpg/revision/latest?cb=20141231230015', 92, 'male','03-19-2022');
+
+INSERT INTO main_app_dog(name, img, age, gender, created_at)
+VALUES ('Scoopy-Doo', 'https://www.thesun.co.uk/wp-content/uploads/2019/11/NINTCHDBPICT000536074229-e1572807808559.jpg', 53, 'male','03-19-2022');
+
+INSERT INTO main_app_dog(name, img, age, gender, created_at)
+VALUES ('Courage', 'https://i.pinimg.com/originals/d9/80/18/d9801898d69e05a9e6c2ea9d42f479ea.png', 26, 'male','03-19-2022');
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 
+
 # Create your views here.
 
 class Home(TemplateView):
